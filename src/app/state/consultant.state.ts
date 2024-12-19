0a4d7d98f5517836bcc2c078339ae6c4554dd5e4
Mariyan <mariyan.kamenov@nuffieldhealth.com>
2024-12-19 22:57:54 +0000

    refactor: :art: upgrade to a18
    upgrade to a18

--- src/app/state/consultant.state.ts
+++ src/app/state/consultant.state.ts
@@ -3,19 +3,19 @@ import { IConsultant } from '../../models';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { ConsultantsService } from 'src/services';
+import { ConsultantsService } from '../../services';
 import { LoadList, ToggleOrder } from './consultant.actions';
 
 interface IConsultantsModel {
   consultants: IConsultant[];
-  isAscending:boolean
+  isAscending: boolean
 }
 
 @State<IConsultantsModel>({
   name: 'consultants',
   defaults: {
     consultants: [],
-    isAscending:true
+    isAscending: true
   },
 })
 @Injectable()
@@ -30,7 +30,7 @@ export class ConsultantsState {
     return state.isAscending;
   }
 
-  public constructor(	private consultantsService: ConsultantsService) {}
+  public constructor(private consultantsService: ConsultantsService) { }
 
   @Action(LoadList)
   public loadList({
@@ -38,9 +38,10 @@ export class ConsultantsState {
   }: StateContext<IConsultantsModel>): Observable<IConsultant[]> {
     return this.consultantsService.consultantstems$.pipe(
       tap((result: IConsultant[]) => {
+        console.log('test', result)
         patchState({
-          consultants: result.sort((a,b) => {
-            
+          consultants: result.sort((a, b) => {
+
             if (a.firstname < b.firstname) {
               return -1;
             }
@@ -48,7 +49,7 @@ export class ConsultantsState {
               return 1;
             }
             return 0;
-          } ),
+          }),
         });
       })
     );
@@ -57,20 +58,20 @@ export class ConsultantsState {
   @Action(ToggleOrder)
   toggleOrder(
     { getState, setState }: StateContext<IConsultantsModel>,
-    {isAscending}: ToggleOrder) {
-      const state = getState();
-      state.consultants.sort((a,b) => {        
-        if (a.firstname < b.firstname) {
-          return isAscending ? -1 : 1;
-        }
-        if (a.firstname > b.firstname) {
-          return isAscending ? 1 : -1;
-        }
-        return 0;
-      } ),
+    { isAscending }: ToggleOrder) {
+    const state = getState();
+    state.consultants.sort((a, b) => {
+      if (a.firstname < b.firstname) {
+        return isAscending ? -1 : 1;
+      }
+      if (a.firstname > b.firstname) {
+        return isAscending ? 1 : -1;
+      }
+      return 0;
+    }),
       setState({
-      consultants: [...state.consultants],
-      isAscending:isAscending
-    });
+        consultants: [...state.consultants],
+        isAscending: isAscending
+      });
   }
 }
--- src/services/consultants.service.ts
+++ src/services/consultants.service.ts
@@ -1,19 +1,24 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { ApiBaseService } from './api-base.service';
 import { map } from 'rxjs/operators';
-import {ConsultantDTO} from './../models'
+import { ConsultantDTO } from './../models'
 
 @Injectable({
-	providedIn: 'root',
+  providedIn: 'root',
 })
 export class ConsultantsService {
-	private readonly apiUrl = 'https://search-api.swiftype.com/api/v1/public/engines/search.json?engine_key=sR_cCweEaptts3ExMPzv&page=1&per_page=20';
+  private readonly apiUrl = 'https://search-api.swiftype.com/api/v1/public/engines/search.json?engine_key=sR_cCweEaptts3ExMPzv&page=1&per_page=20';
+  private httpService = inject(ApiBaseService);
 
-	public constructor(private http: ApiBaseService) {}
-
-	/**
-	 * Gets: gets list of consultants ,
-	 */
-	public consultantstems$ =  this.http.get<any[]>(this.apiUrl).pipe(map((data:any) => data.records.page.map(item => new ConsultantDTO(item)
-	)));
+  constructor() {
+    this.httpService.get(this.apiUrl)
+      .subscribe(data => {
+        console.log('DATA', data)
+      });
+  }
+  /**
+   * Gets: gets list of consultants ,
+   */
+  public consultantstems$ = this.httpService.get<any[]>(this.apiUrl).pipe(map((data: any) => data.records.page.map(item => new ConsultantDTO(item)
+  )))
 }
