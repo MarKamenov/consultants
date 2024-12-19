f83b7953186a0052779666bc897d2d85347967d9
Mariyan <mariyan.kamenov@nuffieldhealth.com>
2024-12-19 22:04:40 +0000

    refactor: :arrow_up: upgrade to a16
    upgrade to a16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { ConsultantsState } from './state/consultant.state';
 import { NgxsModule } from '@ngxs/store';
+import { NgxsFormPluginModule } from '@ngxs/form-plugin';
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
+import { NgxsRouterPluginModule } from '@ngxs/router-plugin';
+import { NgxsStoragePluginModule } from '@ngxs/storage-plugin';
+import { NgxsWebSocketPluginModule } from '@ngxs/websocket-plugin';
 
 @NgModule({
   declarations: [
@@ -18,6 +23,12 @@ import { NgxsModule } from '@ngxs/store';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgxsModule.forRoot([], { developmentMode: /** !environment.production */ false }),
+    NgxsFormPluginModule.forRoot(),
+    NgxsLoggerPluginModule.forRoot(),
+    NgxsRouterPluginModule.forRoot(),
+    NgxsStoragePluginModule.forRoot({ keys: '*' }),
+    NgxsWebSocketPluginModule.forRoot(),
   ],
   bootstrap: [AppComponent]
 })
